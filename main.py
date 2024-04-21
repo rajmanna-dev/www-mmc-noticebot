72309acab07300cf8ce01ce2c33e2de4443bd6f7
Raj <raj2021manna@gmail.com>
2024-04-21 17:21:23 +0530

    second commit - Add ThreadPoolExecutor

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import requests
 import schedule
 import pdfplumber
 from time import sleep
-from bs4 import BeautifulSoup
-from email.mime.multipart import MIMEMultipart
+from bs4 import BeautifulSoup # type: ignore
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from concurrent.futures import ThreadPoolExecutor
 
 import config
 
 previous_notice = []
 
 
+# Send bulk mails
 def send_mail(notice_title, notice_msg, peoples):
     sender_email = config.FROM
     password = config.PASSWORD
@@ -32,8 +34,10 @@ def send_mail(notice_title, notice_msg, peoples):
             text = message.as_string()
             server.sendmail(sender_email, receiver_email, text)
             print(f'sent to {receiver_email}')
+            sleep(5) # sleep for 5 sec
 
 
+# Get text from notice pdf
 def extract_data_from_pdf(file_link):
     r = requests.get(file_link)
     with open('temp.pdf', 'wb') as f:
@@ -46,32 +50,40 @@ def extract_data_from_pdf(file_link):
     return text
 
 
+# Process the first row of notice table
 def process_table_rows(row):
-    # Notice title
-    notice_title = row.select_one('td:nth-of-type(2)').get_text()
-    # Notice link
-    notice_path = config.DOMAIN + row.select_one('td:nth-of-type(3) a')['href'].replace(' ', '%20')
+    notice_title = row.select_one('td:nth-of-type(2)').get_text() # Get notice title
+    notice_path = config.DOMAIN + row.select_one('td:nth-of-type(3) a')['href'].replace(' ', '%20') # Get notice link
     return notice_title, notice_path
 
 
+# Scrape the student notice page
 def scrape_notice():
     url = config.NOTICE_URL
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    tr = soup.find_all('tr')[1]
+    tr = soup.find_all('tr')[1] # Get the first row
 
     global previous_notice
     if tr != previous_notice:
         notice_title, notice_link = process_table_rows(tr)
         notice_text = extract_data_from_pdf(notice_link)
+        # TODO Emails should be dynamic
         send_mail(notice_title, notice_text, ['rajmanna7734@gmail.com', 'raj9163cu@gmail.com'])
         previous_notice = tr
     else:
         print("Notice already sent!")
 
 
+# Main
+def main():
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        schedule.every().hour.do(scrape_notice)
+        while True:
+            schedule.run_pending()
+            sleep(60)
+
+
 if __name__ == '__main__':
-    schedule.every().hour.do(scrape_notice)
-    while True:
-        schedule.run_pending()
-        sleep(900)
+    main()
+    
