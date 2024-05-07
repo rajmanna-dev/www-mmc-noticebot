c51ca17f5e402c84bb17586c75044c62c1fc654d
Raj <raj2021manna@gmail.com>
2024-05-07 12:08:37 +0530

    seperate alert messages into message_style.py

---
+++ message_style.py
@@ -0,0 +1,5 @@
+verification_success = "<div style='text-align:center; margin-top: 5rem;'><h1 style='color: #5cb85c;'>Email is verified successfully.</h1><br><h2>Thanks for Subscribing Us.</h2></div>"
+
+verification_link_expired = "<div style='text-align:center; margin-top: 5rem;'><h1 style='color: #d9534f;'>Verification link has expired. </h1><br><h2>Please request a new verification email.</h2></div>"
+
+invalid_token = "<div style='text-align:center; margin-top: 5rem;'><h1 style='color: #d9534f;'>Invalid token!</h1></div>"
