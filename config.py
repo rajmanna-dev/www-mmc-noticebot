e3112328b888f52856368187725a50190dba8afb
Raj <raj2021manna@gmail.com>
2024-05-12 21:53:37 +0530

    add config file

---
+++ config.py
@@ -0,0 +1,8 @@
+MAIL_PORT=587
+MAIL_USE_TLS=True
+FLASK_ENV='development'
+MAIL_SERVER='smtp.gmail.com'
+PASSWORD='password'
+FROM='your_email_address'
+SECRET_KEY='abcd'
+MONGODB_URL='mongodb_database_url'
